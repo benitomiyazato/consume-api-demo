82632cc22268ed305739abbaa62aa46dac82f327
bemiyaz <benito.miya172@gmail.com>
2023-02-02 13:11:12 -0300

    character/id endpoint

--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
-const express = require('express');
+const express = require("express");
+const axios = require("axios");
 const app = express();
 
-app.get('/api/character', (req, res) => {
-    res.send({message: 'Hello, world!'});
-})
+app.get("/api/character/:id", async (req, res) => {
+    const { id } = req.params;
+  try {
+    const { data } = await axios.get(`https://rickandmortyapi.com/api/character/${id}`);
+    res.send(data);
+  } catch (error) {
+    res.send({error : error.message})
+  }
+});
 
 app.listen(3000);
+
+/**
+ * id
+ * name
+ * status
+ * species
+ * origin (nao tem que ser o objkecto e sim diretamente a propriedade name)
+ * image
+ * episode (quantidade de que tem nesse array e retornar a quantidade ao invés de o array de string)
+ * created
+ */
+
+/**
+ * fazer rota de filtro por personagem baseado no id
+ */
